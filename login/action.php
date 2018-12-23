f03200569d0346929ebf2c5a8e218245c23c18ef
root <root@localhost.localdomain>
2018-12-23 11:49:32 +0530

    jj

--- login/action.php
+++ login/action.php
@@ -2,6 +2,8 @@
 
 include("flag.php");
 
+print_r($_POST["passwd"]);
+
 if (isset($_POST["passwd"])) {
         if (hash("md5", $_POST["passwd"]) == "0e514198428367523082236389979035")        {
                 echo $flag;
