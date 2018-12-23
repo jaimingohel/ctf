9f69445d7b546c76d4e5b39ace27460e71c738fb
root <root@localhost.localdomain>
2018-12-23 11:24:07 +0530

    jj

--- login/index.php
+++ login/index.php
@@ -1,7 +1,9 @@
 <?php
+
 include("flag.php");
-if (isset($_GET['passwd'])) {
-        if (hash("md5", $_GET['passwd']) == '0e514198428367523082236389979035')        {
+
+if (isset($_GET["passwd"])) {
+        if (hash("md5", $_GET["passwd"]) == "0e514198428367523082236389979035")        {
                 echo $flag;
         } 
 } else {
